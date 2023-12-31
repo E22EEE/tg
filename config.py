3311ab624c9e836df51df32c982ec068b83f6f2a
raon <91876178+E22EEE@users.noreply.github.com>
2023-12-31 07:56:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,8 @@ class Config(object):
     APP_ID = int(os.environ.get("APP_ID", 26484720))
 
     API_HASH = os.environ.get("API_HASH", "361cfa5b07e7cef672ecd44c0f4e17cb")
+    SESSION = os.environ.get("SESSION", "")
 
+    UBOT = os.environ.get("USER_BOT", "361cfa5b07e7cef672ecd44c0f4e17cb")
+    DEVLOO = int(os.environ.get("ID_BOT", 6213845793))
     
